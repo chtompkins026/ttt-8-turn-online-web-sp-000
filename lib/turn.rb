f58521c1be49cc29f791a2cc14655bc36edbc20b
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-16 17:38:12 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  #get the user input
+  user_input = gets.strip
+  #input to index
+  index = input_to_index(user_input)
+  token = current_player(board)
+  
+  #check for validation
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
