1ea06d7f6b3f414a7b56c92545ae8f2dab5078e8
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-16 17:41:56 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,7 @@ puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
+  puts "Please enter 1-9:" 
   #get the user input
   user_input = gets.strip
   #input to index
